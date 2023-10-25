a7e59f2765917a5489f5ac6adb0cbcf42724493c
John Krueger <107435492+JohnKrueger0@users.noreply.github.com>
2023-10-24 23:54:42 -0500

    TradingView Idea Scraper Update
    Added easier control over what data source we look at. Also more control over the time period searched.  Examples of first 10 pages of both all posts and just US stock posts from last 2 weeks

--- alternative_data/tv_idea_scrape/ideas_scraper.py
+++ alternative_data/tv_idea_scrape/ideas_scraper.py
@@ -3,14 +3,15 @@ import requests
 import csv
 import numpy as np
 from datetime import datetime
+import time
 
 
 #It may be beneficial to strip all html elements (<div>, <span>, <img>) and formating to just keep text
 
 
 #getting the number of posts per page
-def get_post_titles(_page):
-	url = "https://www.tradingview.com/ideas/page-{}/".format(_page)
+def get_post_titles(_page, _src):
+	url = _src.format(_page)
 	page = requests.get(url)
 	soup = bs(page.content, features="lxml")
 	post_titles = soup.find_all(class_='tv-widget-idea__title')
@@ -18,41 +19,39 @@ def get_post_titles(_page):
 
 #Function to return raw content of TV Idea post
 def get_post(_page, _post_title):
-	post_url = "https://www.tradingview.com/chart" + str(_post_title['href'])	
-	post_page = requests.get(post_url)	
+	post_url = "https://www.tradingview.com/chart" + str(_post_title['href'])
+	post_page = requests.get(post_url)
 	post_soup = bs(post_page.content, features="lxml")
 
 	#Unprocessed TV Idea Post Data
 	post_content = post_soup.find_all(class_='tv-chart-view__description')[0].get_text(strip=True, separator='   ').encode("ascii","ignore").decode("utf-8")
-	print(post_content)
 	post_unix = str.split(str(post_soup.find_all(class_='tv-chart-view__title-time')[-1]), 'data-timestamp="')[-1]
 	post_unix = float(str.split(post_unix, '"')[0])
 	post_time = datetime.utcfromtimestamp(post_unix).strftime('%Y-%m-%d %H:%M:%S') #UTC Time String
 
-	return post_url, post_time, post_content
+	return post_url, post_time, post_unix, post_content
 
 #actually collecting data
-def collect_data(_pages):
+def collect_data(_pages, _days_back, _src, _outfile):
 	header_file = ['Post URL, Time (UTC), Post Content']
 	idea_files = []
 	for i in range(_pages):
-		post_titles = get_post_titles(i+1)
+		post_titles = get_post_titles(i+1, _src)
 		for j in range(len(post_titles)):
-			post_url, post_time, post_content = get_post(i, post_titles[j])
-			idea_files.append([post_time, post_url, post_content])
+			print("Page: {} Post:{}".format(i+1, j))
+			post_url, post_time, post_unix, post_content = get_post(i, post_titles[j])
+
+			if (time.time() - post_unix) / 86400000 <= _days_back:
+				idea_files.append([post_time, post_url, post_content])
 
 	#Saving the scraped ideas, note that this overwrites previous data
-	with open('scraped_ideas.csv', 'w', newline='') as myfile:
+	with open(_outfile, 'w', newline='') as myfile:
 		wr = csv.writer(myfile)
 		wr.writerow(header_file)
 
 		for idea in idea_files:
 			print(idea)
-			#print(''.join(idea.encode('unicode_escape').decode('ascii')))
-			#wr.writerow(''.join(idea.encode('unicode_escape').decode('ascii')))
 			wr.writerow(idea)
 
-     #np.savetxt("scraped_ideas.csv", idea_files, delimiter =", ", fmt ='% s')
-
-#Collecting data on first 2 pages of idea posts
-collect_data(2)
+#Collecting data on first 10 pages of stock idea posts from last 14 days
+collect_data(10, 14, "https://www.tradingview.com/markets/stocks-usa/ideas/page-{}/", 'scraped_stock_ideas.csv')
